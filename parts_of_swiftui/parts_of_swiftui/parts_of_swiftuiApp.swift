e535a05d1e0866a1ea7a238796b92fd07c47b9a4
Matias Adrián Molina <61393019+matias-a-m@users.noreply.github.com>
2023-12-15 13:36:21 -0300

    added project

---
+++ parts_of_swiftui/parts_of_swiftui/parts_of_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  parts_of_swiftuiApp.swift
+//  parts_of_swiftui
+//
+//  Created by matias on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct parts_of_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
