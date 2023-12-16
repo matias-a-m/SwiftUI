fa1632b7b4a0a467d1502c37558027aaf4bbd55c
Matias Adrián Molina <61393019+matias-a-m@users.noreply.github.com>
2023-12-16 01:53:30 -0300

    added mapkit

--- uikit_into_swiftui/uikit_into_swiftui/ContentView.swift
+++ uikit_into_swiftui/uikit_into_swiftui/ContentView.swift
@@ -6,16 +6,26 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+struct ContentView: UIViewRepresentable {
+    
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+        let coordinate = CLLocationCoordinate2D(latitude: -40.0000000, longitude: -64.0000000)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 30)
+        
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        uiView.setRegion(region, animated: true)
+        
+    }
+    
+    
+    func makeUIView(context: Context) -> MKMapView {
+        return MKMapView(frame: .zero)
     }
 }
 
